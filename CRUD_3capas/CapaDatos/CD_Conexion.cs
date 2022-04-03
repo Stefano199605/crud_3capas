a2e89bf701b77899dbcb12f854c9504843bc9139
Stefano199605 <kevin.saldana1996@gmail.com>
2022-04-03 15:21:45 -0500

    Crear clase de conexion

---
+++ CRUD_3capas/CapaDatos/CD_Conexion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+   public class CD_Conexion
+    {
+        private SqlConnection Conexion = new SqlConnection("Server=DESKTOP-3LUEQ59;DataBase=Practica_Patrones;UID=sa;password=p0deroso12");
+
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+            {
+                Conexion.Open();
+                
+            }
+            return Conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+            {
+                Conexion.Close();
+            }
+            return Conexion;
+        }
+    }
+}
